7452732de071dc38c42a5bda4dd15ef26ea53aa5
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-11 17:51:37 -0300

    Applied schema on login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { response, request } = require("express"); //IntelliSense
-const { userRegisterSchema } = require("../schemas/auth");
+const { userRegisterSchema, userLoginSchema } = require("../schemas/auth");
 
 const createUser = async (req, res = response) => {
   try {
@@ -12,9 +12,15 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
-  const { email, password } = req.body;
-  res.json({ resMsg: "User Logged in", email, password });
+const loginUser = async (req, res = response) => {
+  try {
+    const value = await userLoginSchema.validateAsync(req.body);
+    res.json({ resMsg: "User Logged in", ...value });
+  } catch (e) {
+    res.status(400).json({
+      message: e.message,
+    });
+  }
 };
 
 const renewToken = (req, res = response) => {
