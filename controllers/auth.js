10f23e68e303dcce2fc7e3c433f2a49994ea9764
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-12 19:03:42 -0300

    Added implementation of encrypt password and JWT generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,65 @@
 const { response, request } = require("express"); //IntelliSense
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const { encryptPassword } = require("../helpers/encryptPassword");
+const { generateJWT } = require("../helpers/jwt");
 
 const createUser = async (req, res = response) => {
   try {
     const user = new User(req.body);
+
+    user.password = encryptPassword(user.password);
+
     await user.save();
-    res.status(201).json({ resMsg: "User created", ...req.body });
+
+    const token = await generateJWT(user.id, user.name);
+    res
+      .status(201)
+      .json({ status: "Completed", uid: user.id, name: user.name, token });
   } catch (e) {
+    console.log(e);
     res.status(500).json({
-      msg: "Please contact an admin",
+      status: "An error occured",
+      errorMessage: "Please contact an admin",
     });
   }
 };
 
 const loginUser = async (req, res = response) => {
-  res.json({ resMsg: "User Logged in", ...req.body });
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(400).json({
+        status: "An error ocurred",
+        errorMessage: "Email doesn't exist",
+      });
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        status: "An error ocurred",
+        errorMessage: "Password is incorrect",
+      });
+    }
+
+    const token = await generateJWT(user.id, user.name);
+
+    res.json({
+      status: "Completed",
+      uid: user.id,
+      name: user.name,
+      token,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      status: "An error occured",
+      errorMessage: "Please contact an admin",
+    });
+  }
 };
 
 const renewToken = (req, res = response) => {
