beeb6fba6990a1a3972800a9a61c6216623d7ce5
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-12 19:02:45 -0300

    Fixed status

--- middlewares/checkEmail.js
+++ middlewares/checkEmail.js
@@ -3,7 +3,8 @@ const User = require("../models/User");
 const checkEmail = async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email }).exec();
   if (user) {
-    res.status(500).json({
+    res.status(400).json({
+      status: "An error ocurred",
       errorMessage: "An user is using this email",
     });
   } else next();
